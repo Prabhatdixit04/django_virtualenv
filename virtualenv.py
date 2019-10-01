326ebd4fd01a6f9f04e6147419445af090b0b170
prapcode <56020994+prapcode@users.noreply.github.com>
2019-10-01 14:24:19 +0530

    Create virtualenv.py

---
+++ virtualenv.py
@@ -0,0 +1,14 @@
+# installing virtual env wrapper
+pip install virtualenvwrapper
+# creating virtualenv
+mkvirtualenv virtualenv_name
+# installing django
+pip instal django
+#start django project
+django-admin startproject project_name
+# to enter in project
+cd project_name
+# to start an app in project
+python manage.py startapp app_name
+# to run a server
+python manage.py runserver
